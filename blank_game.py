eae89e8969df113752687e1877ca04509b9f5550
Mygan <ma2695@nau.edu>
2017-09-11 09:47:34 -0700

    Add files via upload

--- blank_game.py
+++ blank_game.py
@@ -1,9 +1,14 @@
-import pygame, sys
+import pygame
+import sys
+import os
 from pygame.locals import *
-
 pygame.init()
+
+
 DISPLAYSURF = pygame.display.set_mode((500,400))
-pygame.display.set_caption('Hello World!')
+pygame.display.set_caption('Doggie!')
+FPS= 30
+fpsClock = pygame.time.Clock()
 
 #colors
 black = (0, 0, 0)
@@ -14,10 +19,20 @@ yellow = (255, 255, 0)
 green = (0, 255, 0)
 
 #Scenery
+#while True:
 DISPLAYSURF.fill(sky_blue)
 sun = pygame.draw.circle(DISPLAYSURF, yellow, (400, 50), 30, 0)
 landscape = pygame.draw.rect(DISPLAYSURF, green, (0, 350, 500, 400))
+dogImg = pygame.image.load('C:/Users/Mygan/AppData/Local/Programs/Python/Python36-32/Lib/site-packages/catndog/png/dog/Run (6).png')
+dogImg = pygame.transform.scale(dogImg, (60, 80))
+DISPLAYSURF.blit(dogImg,(0,280))
+pygame.display.flip()
+#c.tick(3)
 
+
+#dogx = 10
+#dogy = 10
+#direction = 'right'
 
 #continuous running loop
 while True:
@@ -26,3 +41,4 @@ while True:
             pygame.quit()
             sys.exit()
     pygame.display.update()
+    fpsClock.tick(FPS)
